9440b99087daefb555b98c25899f6f07b1e5b50e
Utkarsh Gupta <utkarshg@lambdatest.com>
2022-03-21 11:54:58 +0530

    removed filter from option ts

--- src/options.ts
+++ src/options.ts
@@ -297,7 +297,6 @@ function normalizeOptions(input?: Partial<NexeOptions>): NexeOptions {
   }
 
   Object.keys(alias)
-    .filter((x) => true)
     .forEach((x) => delete opts[x])
 
   return options
